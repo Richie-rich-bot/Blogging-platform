b08b860b92b320b3f1de1b4501d86c9e5c435ffc
richie <oftsurich@gmail.com>
2024-01-01 15:29:36 +0000

    Added user authentication

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager,AbstractBaseUser,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
 
 class UserManager(BaseUserManager):
     def create_user(self,email,password = None, **extra_fields):
@@ -27,3 +29,4 @@ class User(AbstractBaseUser,PermissionsMixin):
     is_staff = models.BooleanField(default = False)
     objects = UserManager()
     USERNAME_FIELD = 'email'
+
